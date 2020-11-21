cdfc9d6b331ecfe1ee7aa2e057534e92060be9de
Sergey <zvoncov@gmail.com>
2020-11-21 18:27:02 +0300

    create CoreData manager

---
+++ monofin_mvp/CalendarMainModule/Presenter/CalendarMainPresenter.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarMainPresenter.swift
+//  monofin_mvp
+//
+//  Created by Sergey on 21.11.2020.
+//
+
+import Foundation
+
+protocol CalendarOutProtocol: class {
+    func success()
+    func failure(error: Error)
+}
+
+protocol CalendarInProtocol: class {
+    
+}
