a1c7c277969c69093c701ab020da48571ebdbd79
Sergey <zvoncov@gmail.com>
2020-11-21 19:39:16 +0300

    create basic router and assembly module builder and add navigation View Controller

--- monofin_mvp/Router/Router.swift
+++ monofin_mvp/Router/Router.swift
@@ -6,3 +6,33 @@
 //
 
 import Foundation
+import UIKit
+
+protocol RouterInputProtocol: class {
+    var navigationVC: UINavigationController? { get set }
+    var assemblyBuilder: AssemblyBuilderProtocol? { get set }
+}
+
+protocol RouterOutputProtocol: RouterInputProtocol {
+    func initialViewController()
+}
+
+class Router: RouterOutputProtocol {
+    
+    var navigationVC: UINavigationController?
+    var assemblyBuilder: AssemblyBuilderProtocol?
+    
+    init(navigationVC: UINavigationController, assemblyBuilder: AssemblyBuilderProtocol){
+        self.navigationVC = navigationVC
+        self.assemblyBuilder = assemblyBuilder
+    }
+    
+    func initialViewController() {
+        if let navigationVC = navigationVC {
+            guard let calendarMainModule = assemblyBuilder?.createCalendarMainModule(router: self) else { return }
+            navigationVC.viewControllers = [calendarMainModule]
+        }
+    }
+    
+    
+}
